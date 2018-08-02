1391eb55c2aedc218314714f8b7de689e59c6a44
Oleksii Antypov <echo@wivern.co.uk>
2018-08-02 12:38:48 +0200

    Port from official js sdk

---
+++ src/Webflow/WebflowException.php
@@ -0,0 +1,8 @@
+<?php
+
+class WebflowException extends Exception {
+   public function __construct(string $argument){
+     $this->message = "Argument '{$argument}' is required but was not present";
+     return $this;
+   }
+}
