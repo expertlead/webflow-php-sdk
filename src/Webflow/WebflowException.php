4164608f2b1ac119664905ac6e76ede9d7d75572
johncorrelli <john@teamgantt.com>
2018-12-17 15:48:18 -0500

    Add use Exception statement to WebflowException
    Prevents fatal error reported in #1

--- src/Webflow/WebflowException.php
+++ src/Webflow/WebflowException.php
@@ -2,6 +2,8 @@
 
 namespace Webflow;
 
+use Exception;
+
 class WebflowException extends Exception {
    public function __construct(string $argument){
      $this->message = "Argument '{$argument}' is required but was not present";
